d2d122f556e4c009f2cc9098519c927b459833fa
chasereske <chasereske@gmail.com>
2021-03-02 19:57:41 -0500

    Connection string was stored in the config folder and replaced in the index.js file so as to not show our username and password.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const config = require('config');
 
-mongoose.connect('mongodb+srv://chase123:IamInnovation33!!@cluster0.jxwul.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-{ useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(
+    config.get('mongoURI'),
+    { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('Connected to MongoDB...'))
 .catch((err) => console.log(`Could not connect to MongoDB. ERROR: ${err}`));
